02a6c1cff1672b6ac30e455cb349ccf37c5e7517
Christian Patiño <christianpg@gmail.com>
2025-11-13 09:09:42 -0500

    Add the photo, summary and links

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,46 @@
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import profileImage from "/assets/images/profile.png";
+import "./App.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
 
 function App() {
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
+      <header>
+        <div>
+          <img
+            src={profileImage}
+            className="profilePhoto"
+            alt="Christian's photo"
+          />
+        </div>
+        <h1>Christian Patiño</h1>
+        <h2>Senior Frontend Developer</h2>
+      </header>
+      <main>
+        <p>
+          Systems engineer with 9+ years of experience focused on front-end
+          software development using React and Typescript. Extensive background
+          in the full software development life cycle including requirements
+          definition, evaluation, design, implementation, testing, and
+          maintenance.
+        </p>
+        <h3>WIP</h3>
+      </main>
+      <footer>
+        <a
+          href="https://www.linkedin.com/in/christianpg"
+          target="_blank"
+          title="linkedin"
+        >
+          <FontAwesomeIcon icon={faLinkedin} size="3x" />
         </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+        <a href="https://github.com/ChristianPG" target="_blank" title="Github">
+          <FontAwesomeIcon icon={faGithub} size="3x" />
         </a>
-      </div>
-      <h1>Christian Patiño</h1>
-      <h2>Senior Frontend Developer</h2>
-      <h3>WIP</h3>
-      <a href="https://www.linkedin.com/in/christianpg" target="_blank">
-        LinkedIn Profile
-      </a>
+      </footer>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
