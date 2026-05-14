0badbb3619e320546bd43f36a4e063073e5add0e
Christian Patiño <christianpg813@gmail.com>
2026-05-14 17:35:36 -0500

    style: Update the general layout

--- src/App.tsx
+++ src/App.tsx
@@ -6,30 +6,29 @@ import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
 function App() {
   return (
     <>
-      <header className="mainHeader">
-        <div>
-          <img
-            src={profileImage}
-            className="profilePhoto"
-            alt="Christian's photo"
-          />
-        </div>
-        <h1>Christian Patiño</h1>
-        <h2>Senior Software Engineer</h2>
-      </header>
-      <main className="mainSection">
-        <p>
-          Software engineer with 10+ years of experience as a full stack
-          developer, specializing in frontend development with React,
-          TypeScript, and scalable UI architectures, complemented by backend
-          expertise in Node.js. Proven track record of improving system
-          performance and leading successful projects from concept to delivery.
-          Strong collaborator in cross-functional teams, with a focus on
-          building high-quality, maintainable solutions and continuously
-          expanding technical expertise.
-        </p>
-        <h3>WIP</h3>
-      </main>
+      <div className="mainLayout">
+        <main className="mainSection">
+          <h1>Christian Patiño</h1>
+          <h2>
+            <i>Senior Software Engineer</i>
+          </h2>
+          <p>
+            Software engineer with 10+ years of experience as a full stack
+            developer, specializing in frontend development with React,
+            TypeScript, and scalable UI architectures, complemented by backend
+            expertise in Node.js. Proven track record of improving system
+            performance and leading successful projects from concept to
+            delivery. Strong collaborator in cross-functional teams, with a
+            focus on building high-quality, maintainable solutions and
+            continuously expanding technical expertise.
+          </p>
+        </main>
+        <img
+          src={profileImage}
+          className="profilePhoto"
+          alt="Christian's photo"
+        />
+      </div>
       <footer className="mainFooter">
         <a
           href="https://www.linkedin.com/in/christianpg"
